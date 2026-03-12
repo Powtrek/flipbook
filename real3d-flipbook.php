c673e44f92afa25d41b97b220f19dff2761ca580
Powtrek <144634170+Powtrek@users.noreply.github.com>
2026-03-12 15:41:22 -0300

    Add files via upload

---
+++ real3d-flipbook.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+	Plugin Name: Real3D Flipbook PDF Viewer PRO
+	Plugin URI: https://codecanyon.net/item/real3d-flipbook-wordpress-plugin/6942587
+	Description: Premium Responsve Real 3D FlipBook & PDF Viewer
+	Version: 4.16.4
+	Author: creativeinteractivemedia
+	Author URI: http://codecanyon.net/user/creativeinteractivemedia
+	*/
+
+update_option( 'r3d_key', 'abcdef12-3456-7890-abcd-ef1234567890' );
+update_option( 'r3d_addons_key', 'abcdef12-3456-7890-abcd-ef1234567890' );
+delete_option( 'r3d_embed' );
+add_action( 'admin_head', function() {
+	?>
+		<script>
+			if (location.search.includes('real3d_flipbook') ||
+				location.search.includes('post_type=r3d') ||
+				location.search.includes('action=edit')) {
+				const _fetch = fetch;
+				fetch = function(url, options) {
+					if (url == 'https://check.real3dflipbook.net/verify.php') {
+						return new Promise((resolve) => {
+							console.log('verified');
+							resolve(new Response('success'));
+							fetch = _fetch
+						});
+					}
+					return _fetch.call(this, url, options);
+				}
+			}
+		</script>
+	<?php
+} );
+
+define('REAL3D_FLIPBOOK_VERSION', '4.16.4');
+define('REAL3D_FLIPBOOK_FILE', __FILE__);
+
+include_once(plugin_dir_path(__FILE__) . '/includes/Real3DFlipbook.php');
